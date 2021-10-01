df6e6db8639c340089304cfd8eab168a6f757dec
Srujana Penugonda <penugondasrujana3@gmail.com>
2021-10-01 12:52:28 +0530

    Refactor - Updated API call

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [response, setResponse] = useState("");
 
   const getResponse = async () => {
-    const response = await funTranslatorAPI.get("/chef?text=srujana");
+    const response = await funTranslatorAPI.get("/chef?text=" + text);
     setResponse(response.data.contents.translated);
   };
 
